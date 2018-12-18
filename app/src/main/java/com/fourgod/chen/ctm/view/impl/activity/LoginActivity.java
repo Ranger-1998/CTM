bdc068435a97b5fe4cc07fcc7b82cbd1c3bc5486
志平 朱 <18113928120@189.cn>
2018-12-18 21:38:49 +0800

    登陆,注册界面

---
+++ app/src/main/java/com/fourgod/chen/ctm/view/impl/activity/LoginActivity.java
@@ -0,0 +1,38 @@
+package com.fourgod.chen.ctm.view.impl.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.Window;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.fourgod.chen.ctm.R;
+import com.fourgod.chen.ctm.presenter.impl.BasePresenter;
+
+/**
+ * Created by Tolean on 2018/12/18.
+ */
+
+public class LoginActivity extends BaseActivity {
+    TextView mTvRegister;
+    @Override
+    protected BasePresenter getPresenter() {
+        return null;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mTvRegister=findViewById(R.id.login_register);
+        mTvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+            }
+        });
+    }
+}
